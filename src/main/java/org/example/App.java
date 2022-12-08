86a84104bbd297185267b13526eb41a6c4c2e70f
Allen Joseph <allenjos1996@gmail.com>
2022-12-08 09:53:31 +0100

    xml read and write

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,16 @@
 package org.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.example.model.Address;
+import org.example.model.Person;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +19,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+       try(InputStream inputStream = new FileInputStream(new File("C:/Users/allen/Desktop/people.xml"));){
+           ObjectMapper mapper = new XmlMapper();
+
+           TypeReference<List<Person>> typeReference = new TypeReference<List<Person>>(){};
+           List<Person> people = mapper.readValue(inputStream, typeReference);
+           for(Person p: people){
+               System.out.println("Name is: "+p.getFirstName()+" city is "+p.getAddress().getCity()+" first car is: "+p.getCars()[0]+" age is: "+p.getAge());
+           }
+
+           Person person = new Person();
+           person.setFirstName("Jack");
+           person.setLastName("Ryan");
+           person.setAge(29);
+           person.setAddress(new Address("12 Street", "Kirkland", "98052"));
+           person.setCars(new String[]{"Toyota Corolla", "Honda Accord", "Honda Civic"});
+
+           mapper.writeValue(new File("C:/Users/allen/Desktop/peopleOut.xml"), person);
+
+       }catch (Exception ex){
+           ex.printStackTrace();
+       }
+
     }
 }
